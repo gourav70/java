5f36af790a60492dc8164665cd6c5698f7924416
gourav70 <64587103+gourav70@users.noreply.github.com>
2024-06-06 19:40:43 +0530

    Add files via upload

---
+++ Ifstatement.java
@@ -0,0 +1,17 @@
+package ifstatement;
+
+public class Ifstatement {
+
+	public static void main(String[] args) {
+		  int i = 17;
+		  
+	        if (i < 15)
+	        {
+	            System.out.println("10 is less than 15");
+	 
+	}
+	        System.out.println("Outside if-block");
+
+	}
+
+}
